bd34efc9283c93eeaa2963834524f747f432f785
koroteevmv <koroteevmv@gmail.com>
2019-03-28 14:37:36 +0400

    Code prepared

--- app/src/main/java/com/example/twoactivity/MainActivity.java
+++ app/src/main/java/com/example/twoactivity/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.twoactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnActTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                // TODO Call second activity
+                break;
+            default:
+                break;
+        }
     }
 }
